e05fa61df6f58c247d4fbec3b09365aa5c2ac46a
Bruno <brnonobre@gmail.com>
2025-07-11 09:22:22 -0400

    formatacao de strings
    aprendendo sobre fstrings

---
+++ aulas/aula12.py
@@ -0,0 +1,18 @@
+nome = 'Bruno'
+altura = 1.83
+peso = 69
+
+imc = peso // (altura * 2)
+
+linha_1 = f'{nome} tem {altura:,.2f} de altura'
+linha_2 = f'Pesa {peso} quilos e seu IMC eh'
+linha_3 = f'{imc:.2f}'
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+
+
+# print(nome, 'tem:', altura, 'de altura')
+# print('Pesa:', peso, 'kg e seu IMC eh:')
+# print(imc)
