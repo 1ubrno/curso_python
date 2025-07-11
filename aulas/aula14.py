3b7906b4a6a267641e2d80e4534e9328096a18f4
Bruno <brnonobre@gmail.com>
2025-07-11 11:42:41 -0400

    funcao input

---
+++ aulas/aula14.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome? ')
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro numero: '))
+
+res = int(numero_1) + int(numero_2)
+
+print(f'A soma do numero eh: {res}')
