af7b67227c6bd6bd291493e69fd8f44cf97f24b2
Bruno <brnonobre@gmail.com>
2025-07-13 12:53:26 -0400

    formatacao de strings

---
+++ aulas/aula22.py
@@ -0,0 +1,17 @@
+'''
+Formatacao basica de strings
+s - string
+d - int
+f - float
+.<numero de digitos>f
+x ou X (hexadecimal)
+(Caractere) (><) (quantidade)
+>- Esquerda
+<- Direita
+Sinal - + ou -
+Ex. 0>-100,1f
+conversation flags - !r !s !a
+'''
+
+variavel = 'abc'
+print(f'{variavel: >10}')
