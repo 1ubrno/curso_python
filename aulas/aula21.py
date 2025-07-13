ad6720db553551593ce60d6a61624fcba2a55a37
Bruno <brnonobre@gmail.com>
2025-07-13 12:53:07 -0400

    interpolacao de strings

---
+++ aulas/aula21.py
@@ -0,0 +1,11 @@
+'''
+interpolacao de strings
+s - string
+d e i - int
+f - float
+x e X - hexadecimal (ABCDEF0123456)
+'''
+nome = 'Bruno'
+preco =1000.43243
+variavel = '%s o preco eh: %.2f' % (nome, preco)
+print(variavel)
