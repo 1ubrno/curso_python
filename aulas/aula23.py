66fff851bb716b16b9d9f96ce615a8144cc78ccc
Bruno <brnonobre@gmail.com>
2025-07-13 13:08:46 -0400

    fatiamento de strings

---
+++ aulas/aula23.py
@@ -0,0 +1,11 @@
+'''
+Fatiamento de strings
+012345678
+Ola mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs A funcao len retorna a quantidade
+'''
+
+variavel = 'ola mundo'
+print(len(f'{variavel}'))
