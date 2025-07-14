cfa6fc72f14162452154c92b3d445aca69c0d40a
Bruno <brnonobre@gmail.com>
2025-07-14 17:31:48 -0400

    introducao ao try/except

---
+++ aulas/aula24.py
@@ -0,0 +1,14 @@
+'''
+introducao ao try/except
+try -> tentar executar o codigo
+except -> ocorreu algum erro ao tentar
+'''
+numero_str = input('Vou dobrar o numero que voce digitar')
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} eh {numero_float * 2:.2f}')
+
+# else:
+#     print('Nao eh um numero')
+
