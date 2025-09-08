6689bd9624008f7359f2624afe7f1611d5bc7796
Bruno <brnonobre@gmail.com>
2025-09-08 13:42:04 -0400

    Update aula24.py

--- aulas/aula24.py
+++ aulas/aula24.py
@@ -4,11 +4,13 @@ try -> tentar executar o codigo
 except -> ocorreu algum erro ao tentar
 '''
 
-numero_str = print('Vou dobrar o numero que voce digitar')
+numero = input('Vou dobrar o numero que voce digitar: ')
 
 try:
-    numero_float = float(numero_str)
-    print(f'Float: {numero_float}')
-    print(f'O dobro de {numero_str} eh {numero_float * 2}')
+    print('SRT:', numero)
+    numero_float = float(numero)
+    print('Float:', numero_float)
+    print(f'O dobro de {numero} eh {numero_float * 2 }')
+
 except:
-    print('Nao eh um numero')
+   print('Isso nao eh um numero')
